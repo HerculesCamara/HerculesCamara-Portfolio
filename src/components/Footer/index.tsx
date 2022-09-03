786eac7a6a18d9901d495bdc273acf12256302ce
Hercules Camara <herculescamara2013@gmail.com>
2022-09-03 13:55:35 -0300

    feat: footer links have been updated

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,14 +4,46 @@ export function Footer() {
   return (
     <Container>
       <Content>
-        <a href="https://www.instagram.com/herculescamara_/">Instagram</a>
-        <a href="https://www.linkedin.com/in/herculescamara/">LinkedIn</a>
-        <a href="https://github.com/HerculesCamara">GitHub</a>
-        <a href="https://cssbattle.dev/player/herculescamara">CSS Battle</a>
-        <a href="https://app.rocketseat.com.br/me/hercules-camara">
+        <a
+          href="https://www.instagram.com/herculescamara_/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Instagram
+        </a>
+        <a
+          href="https://www.linkedin.com/in/herculescamara/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          LinkedIn
+        </a>
+        <a
+          href="https://github.com/HerculesCamara"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+        <a
+          href="https://cssbattle.dev/player/herculescamara"
+          target="_blank"
+          rel="noreferrer"
+        >
+          CSS Battle
+        </a>
+        <a
+          href="https://app.rocketseat.com.br/me/hercules-camara"
+          target="_blank"
+          rel="noreferrer"
+        >
           Rocketseat
         </a>
-        <a href="https://www.frontendmentor.io/profile/HerculesCamara">
+        <a
+          href="https://www.frontendmentor.io/profile/HerculesCamara"
+          target="_blank"
+          rel="noreferrer"
+        >
           Front-end Mentor
         </a>
       </Content>
