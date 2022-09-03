09d6ee5eef708bb34396d40e7ecbb2a7b8316876
Hercules Camara <herculescamara2013@gmail.com>
2022-09-03 13:20:05 -0300

    feat: footer links have been updated.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,12 +4,16 @@ export function Footer() {
   return (
     <Container>
       <Content>
-        <a href="">Instagram</a>
-        <a href="">LinkedIn</a>
-        <a href="">GitHub</a>
-        <a href="">CSS Battle</a>
-        <a href="">Rocketseat</a>
-        <a href="">Front-end Mentor</a>
+        <a href="https://www.instagram.com/herculescamara_/">Instagram</a>
+        <a href="https://www.linkedin.com/in/herculescamara/">LinkedIn</a>
+        <a href="https://github.com/HerculesCamara">GitHub</a>
+        <a href="https://cssbattle.dev/player/herculescamara">CSS Battle</a>
+        <a href="https://app.rocketseat.com.br/me/hercules-camara">
+          Rocketseat
+        </a>
+        <a href="https://www.frontendmentor.io/profile/HerculesCamara">
+          Front-end Mentor
+        </a>
       </Content>
     </Container>
   )
